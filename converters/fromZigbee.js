67f0c731f37bc660dd47029c25d05d5f97a31894
Andreas Fritiofson <andreas.fritiofson@unjo.com>
2020-01-25 21:43:57 +0100

    Add custom 0-10 V 2 channel dimmer w/ additional on/off output

--- converters/fromZigbee.js
+++ converters/fromZigbee.js
@@ -3717,6 +3717,30 @@ const converters = {
             }
         },
     },
+    generic_onoff_composite: {
+        cluster: 'genOnOff',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options) => {
+            const key = getKey(model.endpoint(msg.device), msg.endpoint.ID);
+            if (msg.data.hasOwnProperty('onOff')) {
+                const payload = {};
+                payload[key] = {state: msg.data['onOff'] === 1 ? 'ON' : 'OFF'};
+                return payload;
+            }
+        },
+    },
+    generic_brightness_composite: {
+        cluster: 'genLevelCtrl',
+        type: ['attributeReport', 'readResponse'],
+        convert: (model, msg, publish, options) => {
+            const key = getKey(model.endpoint(msg.device), msg.endpoint.ID);
+            if (msg.data.hasOwnProperty('currentLevel')) {
+                const payload = {};
+                payload[key] = {brightness: msg.data['currentLevel']};
+                return payload;
+            }
+        },
+    },
 
     // Ignore converters (these message dont need parsing).
     ignore_onoff_report: {
